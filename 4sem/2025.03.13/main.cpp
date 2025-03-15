722e0c6b7b290e653f5cc75bec18fc058d2e0572
P3Tr0VZ <maxretro4rever@gmail.com>
2025-03-15 06:35:09 +0400

    I shall regret this

--- 4sem/2025.03.13/main.cpp
+++ 4sem/2025.03.13/main.cpp
@@ -7,15 +7,16 @@
 #include <stdexcept>
 #include <array>
 #include <unistd.h>
+#include <vector>
 std::timed_mutex mtx; //Guess, there're time depended operations, not result depended
 
 class TrainStation {
 private:
 	std::pair<char,char> WhosHere{0,0};
 public:
-	char timeForward = 3;
-	char timeIn = 1;
-	char timeBack = 3;
+	char timeForward = 0;
+	char timeIn = 2;
+	char timeBack = 0;
 	const std::string name;
 	void setForwardHere(int id) {
 		if(WhosHere.first != 0 && id != 0 && WhosHere.first != id) {
@@ -33,151 +34,123 @@ public:
 	}
 	TrainStation(const std::string& name):name(name){};
 };
+enum Way{RED, COMMON};
+std::vector stations {
+	std::vector<TrainStation>{
+		TrainStation ("Бакмиль"),
+		TrainStation ("Нариман Нариманов"),
+		TrainStation ("Гянджлик"),
+		TrainStation ("28 Мая"),
+		TrainStation ("Сахил"),
+		TrainStation ("Ичеришехер"),
+	},
+	std::vector<TrainStation>{
+		TrainStation ("Ази Асланов"),
+		TrainStation ("Ахмедлы"),
+		TrainStation ("Халглар Достлугу"),
+		TrainStation ("Нефчиляр"),
+		TrainStation ("Гара Гараев"),
+		TrainStation ("Кероглу"),
+		TrainStation ("Ульдуз"),
 
-std::deque stations {
-	TrainStation ("Бакмиль"),
-	TrainStation ("Ичеришехер"),
-	TrainStation ("Сахил"),
-	TrainStation ("28 Мая"),
-	TrainStation ("Гянджлик"),
-	TrainStation ("Нариман Нариманов"),
+	}
 };
-std::map<char, float> Geolocation;
 
 void PrintGeolocation() {
 	while(true)
 	{
 	std::cout << "---------------------------------\n";
 	for(auto& i: stations) {
-		std::cout << int(i.getIds().first) << ':' << int(i.getIds().second) << '\n';
+		std::cout << "--------------------------------\n";
+
+		for(auto& q: i)
+		{
+			std::cout << int(q.getIds().first) << ':' << int(q.getIds().second) << '\n';
+		}
+		std::cout << "--------------------------------\n";
+
 	}
 	std::cout << "--------------------------------\n";
-	sleep(0.1);
-		system("reset");
+	sleep(1);
+	system("reset");
 }
 }
 
-void PrintSituation() {
-	while(true)
-	{
-	std::cout << "---------------------------------\n";
-	for(auto& i: Geolocation) {
-		std::cout << int(i.first) << ':' << i.second << '\n';
-	}
-	std::cout << "--------------------------------\n";
-	sleep(0.1);
-	}
-}
 class Train {
 public:
 	enum situation{RIDING, STILL};
 	situation TrainSituation = STILL;
 	char id = 0;
 	char location = 0;
+	Way WhereRide = RED;
 	enum direction{FORWARD, BACKWARD};
 	direction TrainDirection = FORWARD;
 	Train(char id1):id(id1){};
-	void TrainMovement(char to) {
+	void TrainMovement(char to, Way ToRide) {
+		if(WhereRide != ToRide)
+		{
+			if(TrainDirection == BACKWARD){
+				stations[WhereRide][location].setBackHere(0);
+				location = stations[ToRide].size()-1;
+			} else {
+				stations[WhereRide][location].setForwardHere(0);
+				location = 0;
+			}
+			WhereRide = ToRide;
+		}
 		std::unique_lock<std::timed_mutex> lock(mtx, std::defer_lock);
-		Geolocation[id] = location;
 		if(location < to) {
 			TrainDirection = FORWARD;
-			while(!lock.try_lock()) {
-				std::cout << "Ему очень херово\n";
-				sleep(1);
-			}			stations[location].setForwardHere(id);
+			stations[WhereRide][location].setForwardHere(id);
 		} else if(location > to){
 			TrainDirection = BACKWARD;
-			while(!lock.try_lock()) {
-				std::cout << "Ему очень херово\n";
-				sleep(1);
-			}			stations[location].setBackHere(id);
+			stations[WhereRide][location].setBackHere(id);
 		} else {
 			TrainSituation = RIDING;
-			if(TrainDirection == FORWARD)
-			{
-				stations[location].setForwardHere(0);
-				lock.unlock();
-				Geolocation[id] = location+0.5;
-				sleep(stations[location].timeForward);
-				Geolocation[id] = location;
-				while(!lock.try_lock()) {
-					std::cout << "Ему очень херово\n";
-					sleep(1);
-				}				stations[location].setBackHere(id);
+			if(TrainDirection == FORWARD) {
+				stations[WhereRide][location].setForwardHere(0);
+				sleep(stations[WhereRide][location].timeForward);
+				stations[WhereRide][location].setBackHere(id);
 			} else {
-				stations[location].setBackHere(0);
-				lock.unlock();
-				Geolocation[id] = location+0.5;
-				sleep(stations[location].timeBack);
-				Geolocation[id] = location;
-				while(!lock.try_lock()) {
-					std::cout << "Ему очень херово\n";
-					sleep(1);
-				}				stations[location].setForwardHere(id);
+				stations[WhereRide][location].setBackHere(0);
+				sleep(stations[WhereRide][location].timeBack);
+				stations[WhereRide][location].setForwardHere(id);
 			}
 		}
 		while(to != location) {
 			TrainSituation = STILL;
-			sleep(stations[location].timeIn);
+			sleep(stations[WhereRide][location].timeIn);
 			TrainSituation = RIDING;
 			if(TrainDirection == FORWARD) {
-				stations[location].setForwardHere(0);
-				lock.unlock();
-				Geolocation[id] = location+0.5;
-				sleep(stations[location].timeForward);
+				stations[WhereRide][location].setForwardHere(0);
+				sleep(stations[WhereRide][location].timeForward);
 				location++;
-				Geolocation[id] = location;
-				while(!lock.try_lock()) {
-					std::cout << "Ему очень херово\n";
-					sleep(1);
-				}				stations[location].setForwardHere(id);
+				stations[WhereRide][location].setForwardHere(id);
 			} else{
-				stations[location].setBackHere(0);
-				lock.unlock();
-				Geolocation[id] = location-0.5;
-				sleep(stations[location].timeBack);
+				stations[WhereRide][location].setBackHere(0);
+				sleep(stations[WhereRide][location].timeBack);
 				location--;
-				Geolocation[id] = location;
-				stations[location].setBackHere(id);
-				while(!lock.try_lock()) {
-					std::cout << "Ему очень херово\n";
-					sleep(1);
-				}
+				stations[WhereRide][location].setBackHere(id);
 			}
 		}
-		sleep(stations[location].timeIn);
+		sleep(stations[WhereRide][location].timeIn);
 	}
 };
 
-
-std::array<Train,7> trains {1,2,3,4,5,6,7};
-
 void CircleRun(Train train) {
-	while(true)
-	{
-		train.TrainMovement(5);
-		/*train.TrainMovement(5);
-		train.TrainMovement(0);
-		train.TrainMovement(0);*/
-	}
+		train.TrainMovement(5,RED);
+		train.TrainMovement(5,RED);
+		train.TrainMovement(0,RED);
+		train.TrainMovement(0, COMMON);
+		train.TrainMovement(0, COMMON);
+		train.TrainMovement(6, COMMON);
+		train.TrainMovement(0,RED);
+		train.TrainMovement(5,RED);
 }
-
-void BeautifulRun() {
-	std::thread t3(PrintGeolocation);
-	std::thread t1(CircleRun, trains[0]);
-	sleep(1);
-	std::thread t2(CircleRun, trains[1]);
-	sleep(1);
-	std::thread t4(CircleRun, trains[2]);
-	t1.join();
-	t2.join();
-	t3.join();
-	t4.join();
-}
-
 
 int main() {
-	BeautifulRun();
-	return 0;
+	std::thread t3(PrintGeolocation);
+	std::thread t1(CircleRun, Train(1));
+	t3.join();
+	t1.join();
 }
