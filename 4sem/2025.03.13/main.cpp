ef5ce3eae7cc98011bff5f54f15ef597dafa3208
P3Tr0VZ <maxretro4rever@gmail.com>
2025-03-16 20:07:09 +0400

    OMG LET ME DIE 12 HOURS CODING

--- 4sem/2025.03.13/main.cpp
+++ 4sem/2025.03.13/main.cpp
@@ -9,6 +9,8 @@
 #include <chrono>
 #include <memory>
  //Guess, there're time depended operations, not result depended
+
+ std::mutex printer;
 class TrainStation {
 	private:
 	std::pair<char,char> WhosHere{0,0};
@@ -136,7 +138,6 @@ class Train {
 private:
 	std::shared_ptr<std::unique_lock<std::mutex>> ForwardLock;
     std::shared_ptr<std::unique_lock<std::mutex>> BackwardLock;
-	std::shared_ptr<std::mutex> PrintMutex = std::make_shared<std::mutex>();
 public:
 	char id = 0;
 	char location = 0;
@@ -144,149 +145,112 @@ public:
 	enum direction{FORWARD, BACKWARD};
 	direction TrainDirection = FORWARD;
 	Train(char id1, Way ride):id(id1),WhereRide(ride){
-		ForwardLock = std::make_shared<std::unique_lock<std::mutex>>(*stations[ride][location].ForwardMutex, std::defer_lock);
+			ForwardLock = std::make_shared<std::unique_lock<std::mutex>>(*stations[ride][location].ForwardMutex, std::defer_lock);
 	};
+	void ReverseMove()
+	{
+		if(location != 0)
+		{
+			location--;
+		}
+		ForwardLock = NULL;
+		BackwardLock = std::make_shared<std::unique_lock<std::mutex>>(*stations[WhereRide][location].BackwardMutex, std::defer_lock);
+		if(BackwardLock.get()->try_lock())
+		{
+			stations[WhereRide][location].setBackHere(id);
+			TrainDirection = BACKWARD;
+		} else {while(!BackwardLock.get()->try_lock()) { /*std::cout << "Всё будет хорошо\n";*/ std::this_thread::sleep_for(std::chrono::seconds(1));}}
+		std::cout << "Поезд \"" << int(id) << "\" сделал круг и ждёт бомжей на " << stations[WhereRide][location].name << '\n';
+		std::this_thread::sleep_for(std::chrono::seconds(stations[WhereRide][location].timeIn));
+		std::cout << "Поезд \"" << int(id) << "\" дождался и сваливает из " << stations[WhereRide][location].name << '\n';
+		if(BackwardLock.get()->owns_lock())
+		{			
+			stations[WhereRide][location].setBackHere(0);
+			BackwardLock.get()->unlock();
+		} else { throw std::logic_error("KALI LINUX"); }
+		location--;
+		std::this_thread::sleep_for(std::chrono::seconds(stations[WhereRide][location].timeBack));
+	}
 	void TrainMovement(char to, Way ToRide) {
 		if(TrainDirection == FORWARD)
 		{
 			ForwardLock = std::make_shared<std::unique_lock<std::mutex>>(*stations[ToRide][location].ForwardMutex, std::defer_lock);
 		}
-		else {		BackwardLock = std::make_shared<std::unique_lock<std::mutex>>(*stations[ToRide][location].BackwardMutex, std::defer_lock); }
-		if(WhereRide != ToRide)
-		{
-			if(TrainDirection == BACKWARD){
-				if(BackwardLock.get()->owns_lock())
+		
+			while(to != location && TrainDirection == FORWARD) {
+				if(ForwardLock.get()->try_lock())
 				{
-					stations[WhereRide][location].setBackHere(0);
-					BackwardLock.get()->unlock();
-				}
-				else { throw std::logic_error("THIS IS KALi LINUX OS \n");}
-				location = stations[ToRide].size()-1;
-			} else {
-				if(ForwardLock.get()->owns_lock()) {
-					stations[WhereRide][location].setForwardHere(0);
-					ForwardLock.get()->unlock();
-				}
-				else { throw std::logic_error("THIS IS KALi LINUX OS \n");}
-				location = 0;
-			}
-			WhereRide = ToRide;
-		}
-		if(location < to) {
-			TrainDirection = FORWARD;
-			//stations[WhereRide][location].first.setForwardHere(id);
-		} else if(location > to){
-			TrainDirection = BACKWARD;
-			if(stations[WhereRide][location].isOnePlatform())
-			{
-				ForwardLock.get()->lock();
-				stations[WhereRide][location].setForwardHere(id);
-			}
-			//stations[WhereRide][location].first.setBackHere(id);
-		} else {
-			if(TrainDirection == FORWARD) {
-				if(ForwardLock.get()->owns_lock()) {
-					stations[WhereRide][location].setForwardHere(0);
-					ForwardLock.get()->unlock();
-				}
-				else {
-					throw std::logic_error("THIS IS KALi LINUX OS \n");}
-				{
-				std::lock_guard<std::mutex> lock(*PrintMutex.get());
-				std::cout << "Поезд \"" << int(id) << "\" свалил с " << stations[WhereRide][location].name << '\n';
-				}
-				sleep(stations[WhereRide][location].timeForward);
-				BackwardLock.get()->lock();
-				stations[WhereRide][location].setBackHere(id);
-				TrainDirection = BACKWARD;
-			} else {
-				if(!stations[WhereRide][location].isOnePlatform())
-				{
-					if(BackwardLock.get()->owns_lock()) {
-						stations[WhereRide][location].setBackHere(0);
-						BackwardLock.get()->unlock();
-					}	else { throw std::logic_error("THIS IS KALi LINUX OS \n");}
-
-				} else {
-					if(ForwardLock.get()->owns_lock()) {
-						stations[WhereRide][location].setForwardHere(0);
-						ForwardLock.get()->unlock();
-					} else { throw std::logic_error("THIS IS KALi LINUX OS \n");}
-				}
-				{
-					std::lock_guard<std::mutex> lock(*PrintMutex.get());
-				std::cout << "Поезд \"" << int(id) << "\" свалил с " << stations[WhereRide][location].name << '\n';
-				}
-				if(!stations[WhereRide][location].isOnePlatform())
-				{
-					sleep(stations[WhereRide][location].timeForward);
-				} else {
-					sleep(stations[WhereRide][location].timeBack);
-				}
-				ForwardLock.get()->lock();
-				stations[WhereRide][location].setForwardHere(id);
-				TrainDirection = BACKWARD;
-			}
-			{
-				std::lock_guard<std::mutex> lock(*PrintMutex.get());
-			std::cout << "Поезд \"" << int(id) << "\" доехал и ждёт бомжей на " << stations[WhereRide][location].name << '\n';
-			}
-			sleep(stations[WhereRide][location].timeIn);
-			{
-				std::lock_guard<std::mutex> lock(*PrintMutex.get());
-			std::cout << "Поезд \"" << int(id) << "\" дождался и свалил с " << stations[WhereRide][location].name << '\n';
-			}
-		}
-		while(to != location) {
-			if(TrainDirection == FORWARD) {
-				if(ForwardLock.get()->owns_lock()) {
-					stations[WhereRide][location].setForwardHere(0);
-					ForwardLock.get()->unlock();
-				} 				else {" THIS IS KALi LINUX OS \n";}
-
-				if(location != stations[WhereRide].size()-1) {
-					std::lock_guard<std::mutex> lock(*PrintMutex.get());
-					std::cout << "Поезд \"" << int(id) << "\" свалил на " << stations[WhereRide][location].name << '\n';
-				}
-				sleep(stations[WhereRide][location].timeForward);
-				location++;
-				ForwardLock = std::make_shared<std::unique_lock<std::mutex>>(*stations[WhereRide][location].ForwardMutex, std::defer_lock);
-				{
-					std::lock_guard<std::mutex> lock(*PrintMutex.get());
-				std::cout << "Поезд \"" << int(id) << "\" доехал и ждёт бомжей на " << stations[WhereRide][location].name << '\n';
-				}
-				ForwardLock.get()->lock();			
-				stations[WhereRide][location].setForwardHere(id);
-			} else{
-				if(BackwardLock.get()->owns_lock()) {
-					stations[WhereRide][location].setBackHere(0);
-					BackwardLock.get()->unlock();
-				} else { throw std::logic_error("THIS IS KALi LINUX OS \n");}
-
-				if(location != stations[WhereRide].size()-1) {
-						std::lock_guard<std::mutex> lock(*PrintMutex.get());
-					std::cout << "Поезд \"" << int(id) << "\" свалил с на " << stations[WhereRide][location].name << '\n';
-				} sleep(stations[WhereRide][location].timeBack);
-				location--;
-				BackwardLock = std::make_shared<std::unique_lock<std::mutex>>(*stations[WhereRide][location].BackwardMutex, std::defer_lock);
-				{
-					std::lock_guard<std::mutex> lock(*PrintMutex.get());
-				std::cout << "Поезд \"" << int(id) << "\" доехал и ждёт бомжей на " << stations[WhereRide][location].name << '\n';
-				}
-				if(stations[WhereRide][location].isOnePlatform())
-				{
-					ForwardLock.get()->lock();
 					stations[WhereRide][location].setForwardHere(id);
-				} else {
-					BackwardLock.get()->lock();
-					stations[WhereRide][location].setBackHere(id);
+				} else {while(!ForwardLock.get()->try_lock()) 
+					{ 
+						std::lock_guard<std::mutex> lock(printer);
+						/*std::cout << "Всё будет хорошо\n"; */
+						std::this_thread::sleep_for(std::chrono::seconds(1));}}
+				{
+					std::lock_guard<std::mutex> lock(printer);
+					std::cout << "Поезд \"" << int(id) << "\" доехал и ждёт бомжей на " << stations[WhereRide][location].name << '\n';
 				}
+				std::this_thread::sleep_for(std::chrono::seconds(stations[WhereRide][location].timeIn));
+				{
+					std::lock_guard<std::mutex> lock(printer);
+					std::cout << "Поезд \"" << int(id) << "\" дождался и сваливает из " << stations[WhereRide][location].name << '\n';
+				}
+				if(ForwardLock.get()->owns_lock())
+				{			
+					stations[WhereRide][location].setForwardHere(0);
+					ForwardLock.get()->unlock();
+				} else { throw std::logic_error("KALI LINUX"); }
+				std::this_thread::sleep_for(std::chrono::seconds(stations[WhereRide][location].timeForward));
+				location++;
+				ForwardLock = std::make_shared<std::unique_lock<std::mutex>>(*stations[ToRide][location].ForwardMutex, std::defer_lock);
 			}
-			sleep(stations[WhereRide][location].timeIn);
-		}
-	}
-};
 
+			while(to != location && TrainDirection == BACKWARD) {
+				if(location < 0)
+				{
+					location = 0;
+				}
+				if(BackwardLock.get()->try_lock())
+				{
+					stations[WhereRide][location].setBackHere(id);
+				} else {while(!BackwardLock.get()->try_lock()) { /*std::cout << "Всё будет хорошо\n";*/ std::this_thread::sleep_for(std::chrono::seconds(1));}}
+				std::cout << "Поезд \"" << int(id) << "\" доехал и ждёт бомжей на " << stations[WhereRide][location].name << '\n';
+				std::this_thread::sleep_for(std::chrono::seconds(stations[WhereRide][location].timeIn));
+				std::cout << "Поезд \"" << int(id) << "\" дождался и сваливает из " << stations[WhereRide][location].name << '\n';
+				if(BackwardLock.get()->owns_lock())
+				{			
+					stations[WhereRide][location].setBackHere(0);
+					BackwardLock.get()->unlock();
+				} else { throw std::logic_error("KALI LINUX"); }
+				std::this_thread::sleep_for(std::chrono::seconds(stations[WhereRide][location].timeBack));
+				if(location != 0)
+				{
+					location--;
+				}
+				BackwardLock = std::make_shared<std::unique_lock<std::mutex>>(*stations[ToRide][location].BackwardMutex, std::defer_lock);
+			}
+	}
+	void TravelToSinglePlatform() {
+		if(BackwardLock.get()->try_lock())
+		{
+			stations[WhereRide][location].setForwardHere(id);
+		} else {while(!BackwardLock.get()->try_lock()) { std::cout << "Всё будет хорошо\n"; std::this_thread::sleep_for(std::chrono::seconds(1));}}
+		std::cout << "Поезд \"" << int(id) << "\" доехал и ждёт бомжей на " << stations[WhereRide][location].name << '\n';
+		std::this_thread::sleep_for(std::chrono::seconds(stations[WhereRide][location].timeIn));
+		std::cout << "Поезд \"" << int(id) << "\" дождался и сваливает из " << stations[WhereRide][location].name << '\n';
+		if(BackwardLock.get()->owns_lock())
+		{			
+			stations[WhereRide][location].setForwardHere(0);
+			BackwardLock.get()->unlock();
+		} else { throw std::logic_error("KALI LINUX"); }
+		std::this_thread::sleep_for(std::chrono::seconds(stations[WhereRide][location].timeForward));
+		location++;
+		BackwardLock = NULL;
+		TrainDirection = FORWARD;
+		ForwardLock = std::make_shared<std::unique_lock<std::mutex>>(*stations[WhereRide][location].ForwardMutex, std::defer_lock);
+	}
+
+};
 void CircleRun(Train train) {
 	while(true) {
 		train.TrainMovement(stations[RED].size()-1,RED);
@@ -299,44 +263,54 @@ void CircleRun(Train train) {
 }
 
 void CircleRun2(Train train) {
-	while(true) {
-		train.TrainMovement(stations[LIGHT_GREEN].size()-1,LIGHT_GREEN);
-		train.TrainMovement(stations[LIGHT_GREEN].size()-1,LIGHT_GREEN);
-		train.TrainMovement(0, LIGHT_GREEN);
-		train.TrainMovement(0, LIGHT_GREEN);
+	while(true)
+	{
+		train.TrainMovement(stations[LIGHT_GREEN].size(),LIGHT_GREEN);
+		train.ReverseMove();
+		train.TrainMovement(0,LIGHT_GREEN);
+		train.ReverseMove();
 	}
 }
 void CircleRun3(Train train) {
-	while(true) {
+	while(true)
+	{
+		train.TrainMovement(stations[PURPLE].size(),PURPLE);
+		train.ReverseMove();
+		train.TrainMovement(0,PURPLE);
+		train.TravelToSinglePlatform();
+	}						///ФИОЛЕТОВАЯ ЛИНИЯ, В ПЛАНАХ ДОДЕЛАТЬ ВОКЗАЛ  И СМЕНУ ТОЧКИ НАЗНАЧЕНИЯ С ВОКЗАЛА НА ХОДЖАСАН И ОБРАТНО
+
+/*	while(true)
+	{
 		train.TrainMovement(stations[PURPLE].size()-1,PURPLE);
 		train.TrainMovement(stations[PURPLE].size()-1,PURPLE);
-		train.TrainMovement(0, PURPLE);
-	}
+		train.TrainMovement(0,PURPLE);
+	}*/
 }
 
 void CircleRunGreen(Train train) {
-	while(true) {
 		train.TrainMovement(stations[GREEN].size()-1,GREEN);
 		train.TrainMovement(stations[GREEN].size()-1,GREEN);
 		train.TrainMovement(0,GREEN);
 		train.TrainMovement(0, COMMON);
 		train.TrainMovement(0, COMMON);
 		train.TrainMovement(stations[COMMON].size()-1, COMMON);
-	}
 }
 int main() {
 	//std::thread t1(CircleRun2, Train(1, LIGHT_GREEN));
-	//std::thread t2(CircleRun, Train(2, RED));
 	std::thread t1(CircleRun3, Train(1, PURPLE));
-	sleep(1);
-	std::thread t2(CircleRun3, Train(2, PURPLE));
-	sleep(1);
-	std::thread t3(CircleRun3, Train(3, PURPLE));
-	sleep(1);
-	std::thread t4(CircleRun3, Train(4, PURPLE));
-	//t6.join();
+	//std::thread t1(CircleRun2, Train(1, LIGHT_GREEN));
 	t1.join();
+
+	//std::this_thread::sleep_for(std::chrono::seconds(1));
+	//std::thread t1(CircleRun3, Train(2, LIGHT_GREEN));
+	//std::this_thread::sleep_for(std::chrono::seconds(1));
+	//std::thread t1(CircleRun3, Train(3, LIGHT_GREEN));
+	//t6.join();
+	/*t1.join();
 	t2.join();
 	t3.join();
 	t4.join();
+	t5.join();
+	t6.join();*/
 }
